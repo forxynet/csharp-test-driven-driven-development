13409867d63b60c8a40cdaac3f977d4b8a27ce49
Hakan Baştürk <forxynet@gmail.com>
2021-11-04 10:27:23 +0300

    BookDeskModelTests updated

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -5,50 +5,49 @@ using Xunit;
 
 namespace DeskBooker.Web.Pages {
     public class BookDeskModelTests {
+        private Mock<IDeskBookingRequestProcessor> _processorMock;
+        private BookDeskModel _bookDeskModel;
+        private DeskBookingResult _deskBookingResult;
+
+        public BookDeskModelTests() {
+            _processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            _bookDeskModel = new BookDeskModel(_processorMock.Object) {
+                DeskBookingRequest = new Core.Domain.DeskBookingRequest()
+            };
+
+            _deskBookingResult = new DeskBookingResult {
+                Code = DeskBookingResultCode.Success
+            };
+
+            _processorMock.Setup(x => x.BookDesk(_bookDeskModel.DeskBookingRequest))
+               .Returns(_deskBookingResult);
+        }
         //[Fact]
         public void ShouldCallBookDeskMethodOfProcess() {
             // Arrange
-            var processorMock = new Mock<IDeskBookingRequestProcessor>();
-
-            var bookDeskModel = new BookDeskModel(processorMock.Object) {
-                DeskBookingRequest = new Core.Domain.DeskBookingRequest()
-            };
-            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
-               .Returns(new DeskBookingResult {
-                   Code = DeskBookingResultCode.Success
-               });
+           
             // Act
-            bookDeskModel.OnPost();
+            _bookDeskModel.OnPost();
 
             // Assert
-            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), Times.Once);
+            _processorMock.Verify(x => x.BookDesk(_bookDeskModel.DeskBookingRequest), Times.Once);
         }
 
         [Theory]
         [InlineData(1,true)]
         [InlineData(0,false)]
         public void ShouldCallBookDeskMethodOfProcessIfModelIsValid(int expectedBookDeskCalls,bool ismOdelValid) {
-            // Arrange
-            var processorMock = new Mock<IDeskBookingRequestProcessor>();
-
-            var bookDeskModel = new BookDeskModel(processorMock.Object) {
-                DeskBookingRequest = new Core.Domain.DeskBookingRequest()
-            };
-
-            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
-                .Returns(new DeskBookingResult {
-                    Code = DeskBookingResultCode.Success
-                });
-
+           
             if (!ismOdelValid) {
-                bookDeskModel.ModelState.AddModelError("JUstAKey", "AnErrorMessage");
+                _bookDeskModel.ModelState.AddModelError("JUstAKey", "AnErrorMessage");
             }
 
             // Act
-            bookDeskModel.OnPost();
+            _bookDeskModel.OnPost();
 
             // Assert
-            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), 
+            _processorMock.Verify(x => x.BookDesk(_bookDeskModel.DeskBookingRequest), 
                 Times.Exactly(expectedBookDeskCalls));
         }
 
@@ -56,22 +55,24 @@ namespace DeskBooker.Web.Pages {
         public void ShouldAddModelErrorIfNoDeskIsAvailable() {
 
             // Arrange
-            var processorMock = new Mock<IDeskBookingRequestProcessor>();
-
-            var bookDeskModel = new BookDeskModel(processorMock.Object) {
-                DeskBookingRequest = new Core.Domain.DeskBookingRequest()
-            };
-
-            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
-                .Returns(new DeskBookingResult {
-                    Code = DeskBookingResultCode.Success
-                });
+            _deskBookingResult.Code = DeskBookingResultCode.NoDeskAvailable;
 
             // Act
-            bookDeskModel.OnPost();
+            _bookDeskModel.OnPost();
 
             // Assert
-            Assert.DoesNotContain("DeskBookingRequest.Date", bookDeskModel.ModelState);
+            Assert.Contains("DeskBookingRequest.Date", _bookDeskModel.ModelState);
+        }
+
+        [Fact]
+        public void ShouldNotAddModelErrorIfDeskIsAvailable() {
+            // Arrange
+            _deskBookingResult.Code = DeskBookingResultCode.Success;
+            // Act
+            _bookDeskModel.OnPost();
+
+            // Assert
+            Assert.DoesNotContain("DeskBookingRequest.Date", _bookDeskModel.ModelState);
         }
     }
 }
